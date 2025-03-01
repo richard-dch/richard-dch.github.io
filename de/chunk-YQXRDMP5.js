1f9d03a442d445b47d851dd6e2018003fb50fa22
Richard Robu <richard@devcentrehouse.eu>
2025-03-01 21:38:46 +0000

    Revert "Updated UI"
    This reverts commit f07eb64b6bcbadb28ddedb1d011d90bba688ff55.

---
+++ de/chunk-YQXRDMP5.js
@@ -0,0 +1,2 @@
+import{a as e}from"./chunk-5PVX24OM.js";var l={erp:{label:"ERP",url:`/${e.erpTechnology}`,ariaLabel:"See ERP solutions at Dev Centre House Ireland"},crm:{label:"CRM",url:`/${e.crmTechnology}`,ariaLabel:"See CRM solutions at Dev Centre House Ireland"}};export{l as a};
+/**i18n:2959a088a4f851c603ee32029e8942bcc70b3666ad7eada0c74607a8984ba57d*/
